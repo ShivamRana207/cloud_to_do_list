b503442e577a3b62e705dc08f9c625d0ba40fec1
ranashivam207 <ranashivam207@gmail.com>
2022-04-23 14:44:48 +0530

    api end point for the notes and auth

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
   res.send('Hello Shivam')
 })
 
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
