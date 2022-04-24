c409e832d308b7e5f28f7afae97f5a424734d1d8
ranashivam207 <ranashivam207@gmail.com>
2022-04-24 19:40:39 +0530

    making end point to create user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,14 @@
 const express =require('express');
+const User = require('../models/User');
 const router = express.Router();
+const user =  require('../models/User');
 
+// Creatin a user Using : POST at "/api/auth", Who do not need auth.
 router.get('/', (req,res)=>{
-    obj = {
-    a: 'this is for authentication',
-    number: 234234
-    } 
-    res.json(obj)
+  res.send(req.body);
+    const user = User(req.body);
+    console.log(req.body);
+    user.save;
 })
 
 module.exports = router;
