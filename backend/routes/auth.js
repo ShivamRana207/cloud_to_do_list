d2c3a8bddd6f2d66626a9ed43748fbc62169fdec
ranashivam207 <ranashivam207@gmail.com>
2022-04-25 19:51:51 +0530

    trying to fix mongodb

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,11 +4,15 @@ const router = express.Router();
 const user =  require('../models/User');
 
 // Creatin a user Using : POST at "/api/auth", Who do not need auth.
+
+
 router.get('/', (req,res)=>{
   res.send(req.body);
     const user = User(req.body);
     console.log(req.body);
     user.save;
 })
+// trying to remove mongodb error
+
 
 module.exports = router;
