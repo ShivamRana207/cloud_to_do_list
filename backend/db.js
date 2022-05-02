1aa8dd3fa9ab4a0a285e776b98c6e5ad0610171f
ranashivam207 <ranashivam207@gmail.com>
2022-05-02 12:54:56 +0530

    intial commit

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const mongoURI = "mongodb+srv://Shivam:8uTUW4aomz3ac7hy@cluster0.zltdg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const connectToMongo = () => {
-    mongoose.connect(mongoURI, () => {
+    mongoose.connect(mongoURI,  { useNewUrlParser: true, useUnifiedTopology: true, }, () => {
         console.log("Connected to Mongo Successfully");
     })
 }
