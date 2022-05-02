36459c41395afedb5db2331edb127590a094233f
ranashivam207 <ranashivam207@gmail.com>
2022-05-02 13:21:40 +0530

    Adding components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-    <div className="h2">This is front end</div>
-    </div>
+    <>
+    <Navbar/>
+      <div className="h2">This is front end
+      </div>
+    </>
   );
 }
 
